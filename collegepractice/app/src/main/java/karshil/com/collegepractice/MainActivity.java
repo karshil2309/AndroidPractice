a73b9f277ab541327c984989dbd34e18ebd1c656
karshilsheth <karshilsheth@gmail.com>
2020-05-06 22:57:51 +0530

    First Practice on Android!

---
+++ collegepractice/app/src/main/java/karshil/com/collegepractice/MainActivity.java
@@ -0,0 +1,13 @@
+package karshil.com.collegepractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
